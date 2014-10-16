2f1e4504d1267911bc32f35cb974e15a54256ea0
Christopher Anzalone <canzalon@fau.edu>
2014-10-16 02:14:33 -0400

    Uploading problem 2 files

---
+++ canzalon_problem2/GenericStack2.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * Solution: .NET-CLI-process-library-assemblies (assig2.doc)
+ * Project: canzalon_problem2
+ * File/Module: GenericStack2.cs
+ * Author: Christopher Anzalone
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+
+public class OutofRangeArgs : EventArgs
+{
+    private int index;
+    public OutofRangeArgs(int index2)
+    {
+        index = index2;
+    }
+    public int Index
+    {
+        get { return index; }
+    }
+}
+
+public class GenericStack2<T>
+{
+    private T[] s;
+    private int top = -1;
+
+    public delegate void OutofRangeHandler(object sender, OutofRangeArgs e);
+    public event OutofRangeHandler OutofRange;
+
+    public GenericStack2(int size)
+    { s = new T[size]; }
+    public int Push(T x)
+    {
+        if (top == s.Length - 1) return -1;
+        s[++top] = x;
+        return 0;
+    }
+    public int Pop()
+    {
+        if (top == -1) return -1;
+        top--;
+        return 0;
+    }
+    public int TopVal(out T x)
+    {
+        if (top == -1)
+        { x = default(T); return -1; }
+        x = s[top];
+        return 0;
+    }
+
+    /**/
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = top; i >= 0; i--)
+        {
+            yield return s[i];
+        }
+    }
+
+    public T this[int i]
+    {
+        get
+        {
+            if (i <= top && i >= 0)
+            {
+                return s[top - i];
+            }
+            else
+            {
+                OutofRangeArgs e = new OutofRangeArgs(i);
+                OutofRange(this, e);
+                return default(T);
+            }
+        }
+    }
+}
---
+++ canzalon_problem2/IntStackUser.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Solution: .NET-CLI-process-library-assemblies (assig2.doc)
+ * Project: canzalon_problem2
+ * File/Module: IntStackUser.cs
+ * Author: Christopher Anzalone
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace canzalon_problem2
+{
+    class IntStackUser
+    {
+        static void Main()
+        {
+            Console.WriteLine("B. Creates a GenericStack2<int> object.");
+            GenericStack2<int> theStack = new GenericStack2<int>(10);
+
+            Console.WriteLine("A. Registers an anonymous method as a handler for the OutofRange event.");
+            theStack.OutofRange += delegate(object sender, OutofRangeArgs e)
+            {
+                Console.Write("Index " + e.Index + " is out of range.");
+                Console.WriteLine("Terminating the program due to index out of range.");
+                System.Environment.Exit(0);
+            };
+
+            Console.WriteLine("C.Pushes 3, 5, and 7 onto the stack.");
+            theStack.Push(3);
+            theStack.Push(5);
+            theStack.Push(7);
+
+            Console.WriteLine("D. Uses a foreach statement to print the stack elements in reverse.");
+            foreach (int i in theStack)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("E. Prints the element two positions below the top of the stack.");
+            Console.WriteLine(theStack[2]);
+
+            Console.WriteLine("F.Pops all the elements off the stack.");
+            theStack.Pop();
+            theStack.Pop();
+            theStack.Pop();
+
+            Console.WriteLine("G.Tries to print the element two positions below the top of the stack.");
+            Console.WriteLine(theStack[2]);
+        }
+    }
+}
